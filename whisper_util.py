fa1c188cd8fd95cb7decbaa2c50b70d7fdc7cb4d
Dave G <davegutz@users.noreply.github.com>
2023-05-10 03:53:22 -0400

    Update whisper_util.py

--- whisper_util.py
+++ whisper_util.py
@@ -283,10 +283,11 @@ def paragraph(txt_path):
     with open(txt_path, 'w') as file:
         for line in lines:
             file.write(line)
-            if line and line[-1] == '.':
-                file.write('  ')
-            else:
-                file.write(' ')
+            if line:
+                if line[-1] == '.':
+                    file.write('  ')
+                else:
+                    file.write(' ')
     file.close()
 
 
